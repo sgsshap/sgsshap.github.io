a9fc1738d43bcf509d7f0d034a5b525d241b77c7
Chess brain <3690985693@qq.com>
2026-08-10 20:38:56 -0400

    修复百科无法显示的问题
    见标题，不想再打一遍字

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://www.sgsshap.cn:8081',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
